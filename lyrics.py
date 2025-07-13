755d0bbceedb5f8aa1d67ef342778dadba261fa4
pejalgit71 <160094060+pejalgit71@users.noreply.github.com>
2025-07-14 02:10:07 +0800

    Update lyrics.py

--- lyrics.py
+++ lyrics.py
@@ -552,8 +552,8 @@ elif choice == "📍 Performance Venues & Tokens":
         
         # Rename columns *after* all operations are safe
         summary_df.rename(columns={
-        "Member": _["Member"],
-        "TotalEarned": _["TotalEarned"]
+        "Member": _("Member"),
+        "TotalEarned": _("TotalEarned")
         }, inplace=True)
 
 
