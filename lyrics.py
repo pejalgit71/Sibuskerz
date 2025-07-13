d3727674f88544a4ec8f66ea81e10d3a5ce339e2
pejalgit71 <160094060+pejalgit71@users.noreply.github.com>
2025-07-13 14:30:48 +0800

    Update lyrics.py

--- lyrics.py
+++ lyrics.py
@@ -73,7 +73,7 @@ lyrics_df = get_lyrics_df(worksheet)
 
 # --- VIEW LYRICS ---
 if choice == "📖 View Lyrics/Lihat Lirik":
-    st.image("patrick-patrick-star.gif", width=150)
+    st.image("patrick-patrick-star.gif",use_container_width=False)
     st.subheader("Select a song to view lyrics")
 
     lyrics_df_sorted = lyrics_df.sort_values(by='Title')
